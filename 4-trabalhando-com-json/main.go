1635d9526ab787ea5f8f049f56b9d112d30590be
Jonilson Sousa <jonilsonds9@gmail.com>
2025-05-05 16:56:55 -0300

    4-trabalhando-com-json

---
+++ 4-trabalhando-com-json/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Conta struct {
+	Numero int `json:"n"`
+	Saldo  int `json:"s"`
+}
+
+func main() {
+	conta := Conta{Numero: 1, Saldo: 100}
+	res, err := json.Marshal(conta)
+	if err != nil {
+		println(err)
+	}
+	println(string(res))
+
+	err = json.NewEncoder(os.Stdout).Encode(conta)
+	if err != nil {
+		println(err)
+	}
+
+	// jsonPuro := []byte(`{"Numero":2,"Saldo":200}`) // Usando campos diretamente
+	jsonPuro := []byte(`{"n":2,"s":200}`) // Usando tags
+	var contaX Conta
+	err = json.Unmarshal(jsonPuro, &contaX)
+	if err != nil {
+		println(err)
+	}
+	println(contaX.Numero, contaX.Saldo)
+}
