8b28f26b1174e4e2406b51e1019105bd972a9ad7
Jonilson Sousa <jonilsonds9@gmail.com>
2025-05-14 10:17:13 -0300

    Trabalhando com HTTP usando contextos

---
+++ 18-trabalhando-com-http-usando-contextos/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Microsecond)
+	//ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://google.com", nil)
+	if err != nil {
+		panic(err)
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(body))
+}
